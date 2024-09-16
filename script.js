c101550873e923568289598a592d2dbf55ef2b3c
Inshra23 <164750803+Inshra23@users.noreply.github.com>
2024-09-16 15:03:51 +0500

    mypractice

---
+++ script.js
@@ -0,0 +1,80 @@
+//try;;when we not function is not make.. try statement defines a code block to run (to try).
+
+//The catch statement defines a code block to handle any error.
+// try{
+//     console.log("hello world");
+// Registration();
+// console.log("javascript chapters");
+// }
+// catch(err){
+//     console.log(err);
+// }
+
+// function greetWorld() { try {
+//     var greeting = "Hello world!"; aler(greeting);
+    
+  
+//     throw "syntax error"
+// }
+//     catch(err) { alert(err);
+    
+    
+    
+    
+    
+//     }
+// }   
+// greetWorld() 
+// //mostly use in log in sign up...\
+
+
+
+// //throw it is custom error
+
+// function checkPassword() { try {
+//     var pass = document.getElementById("f1").value; if (pass.length < 8) {
+//     throw "Please enter at least 8 characters.";
+    
+
+    
+//     }
+//     if (pass.indexOf(" ") !== -1) {
+//     throw "No spaces in the password, please.";
+    
+    
+//     }
+//     var numberSomewhere = false;
+//     for (var i = 0; i < pass.length; i++) { if (isNaN(pass(i, i+1)) === false) {
+//     numberSomewhere = true; 
+//     break;
+//     }
+// }
+
+    
+//     if (numberSomewhere === false) { throw "Include at least 1 number.";
+    
+
+//     }
+//     }
+    
+    
+//     catch(err) { alert(err);
+    
+    
+//     }
+//     }
+//     // script.js
+// document.addEventListener('DOMContentLoaded', function () {
+//     const passwordInput = document.getElementById('password');
+//     const togglePassword = document.getElementById('togglePassword');
+
+//     togglePassword.addEventListener('click', function () {
+//         // Toggle the type attribute
+//         const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+//         passwordInput.setAttribute('type', type);
+
+//         // Toggle the eye icon
+//         this.classList.toggle('fa-eye-slash');
+//     });
+// });
+
